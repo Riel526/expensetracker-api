104c2c5d3c7c5055351c54a1babeb01c8f652fb8
PETNET-Gabriel Padolina <gabriel.padolina@petnet.com.ph>
2024-06-28 10:27:20 +0800

    configuration of sqlite using sequalize

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 // Dependencies
 
 const express = require("express");
+const bodyParser= require('body-parser');
+const {Sequelize, Model, DataTypes} = require("sequelize")
 
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite3'
+})
+
+sequelize.sync();
 
 // Configuration
-
 const port = 3000;
 const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+// Services
+app.get("/", (req,res)=>{
+  res.json({ message: 'Welcome to expense tracker'})
+})
 
 //Run the server application
 app.listen(port , ()=>{
