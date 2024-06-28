c0757a4386b68aafbd66d4a988795dfa9b774c20
PETNET-Gabriel Padolina <gabriel.padolina@petnet.com.ph>
2024-06-28 11:18:37 +0800

    added a post method

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 // Dependencies
-
 const express = require("express");
-const bodyParser= require('body-parser');
-const {Sequelize, Model, DataTypes} = require("sequelize")
+const bodyParser = require("body-parser");
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
+// Create sequelize instance
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite3'
+    dialect: 'sqlite',
+    storage: './database.sqlite3'
 })
 
-// define models
+// Define models
 class Expense extends Model {}
 
 Expense.init({
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  amount: {
-    type: DataTypes.DECIMAL,
-    allowNull: false,
-  }
-}, { sequelize, modelName: 'expense' })
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    amount: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    }
+}, { sequelize, modelName: 'expense' });
 
 sequelize.sync();
 
@@ -30,15 +30,30 @@ const port = 3000;
 const app = express();
 
 // Middleware
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 // Services
-app.get("/", (req,res)=>{
-  res.json({ message: 'Welcome to expense tracker'})
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Expense Tracker!" });
 })
 
-//Run the server application
-app.listen(port , ()=>{
-  console.log(`server is running at ${port}`);
+// API to return all expense items
+app.get("/expenses", async (req, res) => {
+    const expenses = await Expense.findAll();
+
+    res.json(expenses);
+});
+
+// API to create an expense item
+// { name: "Food", amount: 300.00 }
+app.post("/expenses", async (req, res) => {
+    const expense = await Expense.create(req.body);
+
+    res.json(expense);
+});
+
+// Run the server application
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
 });
